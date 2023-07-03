69c6886a898ff52a82fb3810c3d62bccc82836bb
EgorPashnin201 <119685442+EgorPashnin201@users.noreply.github.com>
2023-07-03 18:41:38 +0300

    Create directory of project

---
+++ synergia-project.loc/Zipterest/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+    <link rel="shortcut icon" href="blocks/header/assets/lo.ico" type="image/x-icon">
+    <link rel="Stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="Header" style="width: 1280px; height: 125px; position: relative">
+    <div class="Header" style="width: 1280px; height: 125px; left: 0px; top: 0px; position: absolute; background: #1F51CF; border-top-left-radius: 45px; border-top-right-radius: 45px"></div>
+    <div class="AccountButton" style="width: 125px; height: 50px; left: 51px; top: 36px; position: absolute">
+        <div class="Background" style="width: 125px; height: 50px; left: 0px; top: 0px; position: absolute; background: #EEEEEE; border-radius: 20px"></div>
+        <div style="left: 18px; top: 7px; position: absolute; color: black; font-size: 30px; font-family: Inter; font-weight: 400; word-wrap: break-word">Войти</div>
+    </div>
+    <div class="Search" style="width: 600px; height: 50px; left: 606px; top: 36px; position: absolute">
+        <div class="SearchRectangle" style="width: 600px; height: 50px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 20px"></div>
+        <div style="left: 26px; top: 5px; position: absolute; color: rgba(0, 0, 0, 0.50); font-size: 30px; font-family: Inter; font-weight: 400; word-wrap: break-word">Поиск</div>
+    </div>
+    </div>
+</body>
